17c294ce21e57d5977c113c8ed8e6012902a6af7
bivarz <paulo.bivar@gmail.com>
2020-07-29 18:13:20 -0300

    :tada: Editing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
 import Header from './components/Header/Header';
 // import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import GlobalStyle from './styles/global';
-import store from './store';
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <GlobalStyle />
-        <Header />
-        <Home />
-      </Provider>
+      <GlobalStyle />
+      <Header />
+      <Home />
     </>
   );
 }
